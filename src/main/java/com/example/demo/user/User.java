4e0f68665ab070d29b76bdaa44fa1b56a08fa4a6
prakash.jhugroo <prakash.jhugroo@agileum.com>
2023-04-03 15:06:08 +0400

    #main

---
+++ src/main/java/com/example/demo/user/User.java
@@ -0,0 +1,41 @@
+package com.example.demo.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "mail")
+    private String mail;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+}
